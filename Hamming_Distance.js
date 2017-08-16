13b97bfad0d33fd5f6755ada6c8cb867beb8f32d
KevinSharma <kevin.sharma@gmail.com>
2017-08-16 16:15:59 -0700

    Code clean up.

--- Hamming_Distance.js
+++ Hamming_Distance.js
@@ -9,7 +9,7 @@ function hammingDistance( x, y ) {
         return 0;
     }
 
-    var bin_1, bin_2;
+    var bin_1 = 0, bin_2 = 0;
     var count = 0;
     var binary = '';
 
@@ -17,22 +17,22 @@ function hammingDistance( x, y ) {
     bin_2 = conv_Dec_toBin( y );
 
     // checks if length of binary for x and y are equal.
-    if( bin_1.length == bin_2.length ){
-        return;
-    }
-    // checks if length of binary of x is greater than y.
-    else if( bin_1.length > bin_2.length ){
-        var add = bin_1.length - bin_2.length;
+    if( bin_1.length != bin_2.length ){
+        var add = 0;
+        // checks if length of binary of x is greater than y.
+        if( bin_1.length > bin_2.length ){
+            add = bin_1.length - bin_2.length;
 
-        bin_2 = '0'.repeat(add) + bin_2;
-    }
-    // checks if length of binary of y is greater than x.
-    else if( bin_2.length > bin_1.length ){
-        var add = bin_2.length - bin_1.length;
+            bin_2 = '0'.repeat(add) + bin_2;
+        }
+        // checks if length of binary of y is greater than x.
+        else{
+            add = bin_2.length - bin_1.length;
 
-        bin_1 = '0'.repeat(add) + bin_1;
+            bin_1 = '0'.repeat(add) + bin_1;
+        }
     }
-    // compares each element of x to y and counts the number of differences.
+    // // compares each element of x to y and counts the number of differences.
     for(var i = 0; i < bin_1.length; i++){
         if(bin_1[i] != bin_2[i]){
             count ++;
@@ -51,11 +51,11 @@ function conv_Dec_toBin(dec1){
         temp = (dec1 % 2).toString();
         binary = temp + binary;
         // removes the decimal of the quotient.
-        dec1 = Math.trunc(dec1/2);
+        dec1 = Math.trunc(dec1 / 2);
     }
 
     return binary;
 }
 
-var result = hammingDistance( 1,4 );
+var result = hammingDistance( 93,73 );
 console.log(result);
