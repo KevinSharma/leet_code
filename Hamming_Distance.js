e643420ed19c8c187b04eb45870e7bd1d40dc0d1
KevinSharma <kevin.sharma@gmail.com>
2017-08-16 15:29:53 -0700

    Hamming Distance solution.

---
+++ Hamming_Distance.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+function hammingDistance( x, y ) {
+
+    if( x == y ){
+        return 0;
+    }
+
+    var bin_1, bin_2;
+    var count = 0;
+    var binary = '';
+
+    bin_1 = conv_Dec_toBin( x );
+    bin_2 = conv_Dec_toBin( y );
+
+    // checks if length of binary for x and y are equal.
+    if( bin_1.length == bin_2.length ){
+        return;
+    }
+    // checks if length of binary of x is greater than y.
+    else if( bin_1.length > bin_2.length ){
+        var add = bin_1.length - bin_2.length;
+
+        bin_2 = '0'.repeat(add) + bin_2;
+    }
+    // checks if length of binary of y is greater than x.
+    else if( bin_2.length > bin_1.length ){
+        var add = bin_2.length - bin_1.length;
+
+        bin_1 = '0'.repeat(add) + bin_1;
+    }
+    // compares each element of x to y and counts the number of differences.
+    for(var i = 0; i < bin_1.length; i++){
+        if(bin_1[i] != bin_2[i]){
+            count ++;
+        }
+    }
+
+    return count;
+};
+
+function conv_Dec_toBin(dec1){
+    var temp = '';
+    var binary = '';
+
+    while( dec1 > 0){
+        // stores remainder as a string.
+        temp = (dec1 % 2).toString();
+        binary = temp + binary;
+        // removes the decimal of the quotient.
+        dec1 = Math.trunc(dec1/2);
+    }
+
+    return binary;
+}
+
+var result = hammingDistance( 1,4 );
+console.log(result);
