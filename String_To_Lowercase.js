c49ac9dd0c07019eafbe22bcf86693ef5ac27e0c
KevinSharma <kevin.sharma@gmail.com>
2018-07-15 00:03:52 -0700

    Getting familiar with toLowerCase js function.

---
+++ String_To_Lowercase.js
@@ -0,0 +1,18 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+var toLowerCase = function(str) {
+
+    return str.toLowerCase();
+
+}
+
+var result = toLowerCase("Hello");
+console.log(result);
+result = toLowerCase("Yes");
+console.log(result);
+result = toLowerCase("No");
+console.log(result);
+result = toLowerCase("ADDVVGadf;lkjvoijer;--94357-89-9831");
+console.log(result);
+result = toLowerCase(",./';<>?:[qwerzxvc,n;lkdsflkjk]\|}A{=-0987654321`T~!@#$%^&*()_+}N'poiu");
+console.log(result);
